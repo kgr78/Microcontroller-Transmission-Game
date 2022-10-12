5360d744db14f1717f05954ba8660d8c7d261329
Kahu Griffin <kgr78@cs18261kq.canterbury.ac.nz>
2022-10-12 19:14:57 +1300

    testing recieving

--- game.c
+++ game.c
@@ -1,14 +1,19 @@
 #include "system.h"
+#include "led.h"
+#include "navswitch.h"
+
+
+
 
 int main (void)
 {
     system_init ();
-
-
-    while (1)
-    {
-
-
-
+    if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+            ir_uart_putc (1);
     }
+    while (1)
+    { 
+        
+    }
+
 }
