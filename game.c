6e3d28c1f4617e73e01a85ba530ef4a7b9c32b5c
Connor Parsons <cpa116@cs18196kq.canterbury.ac.nz>
2022-10-13 17:20:54 +1300

    kahus game.c update

--- game.c
+++ game.c
@@ -2,28 +2,29 @@
 #include "led.h"
 #include "navswitch.h"
 #include "ir_uart.h"
-
+//kahu
 
 
 int main (void)
 {
-    system_init ();
-    ir_uart_init ();
-    int8_t sent = 0;
-    int8_t received = 0;
 
-    while (sent != 1 && received != 1)
-    {
-        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-            ir_uart_putc ('?');
-            sent = 1;
-        }
-        if (ir_uart_read_ready_p ())
-        {
-            if (ir_uart_getc () == "?") {
-                received = 1;
-            }
-        }
-    }
+    // system_init ();
+    // ir_uart_init ();
+    // int8_t sent = 0;
+    // int8_t received = 0;
+    // welcome_screen();
+    // while (sent != 1 && received != 1)
+    // {
+    //     if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
+    //         ir_uart_putc ('?');
+    //         sent = 1;
+    //     }
+    //     if (ir_uart_read_ready_p ())
+    //     {
+    //         if (ir_uart_getc () == "?") {
+    //             received = 1;
+    //         }
+    //     }
+    // }
     
 }
