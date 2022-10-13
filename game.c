0e5e9cf0e18aac6e2a6de9eee56f2dc3570b381b
Connor Parsons <cpa116@cs18268kq.canterbury.ac.nz>
2022-10-13 15:22:24 +1300

    added game.c

--- game.c
+++ game.c
@@ -1,19 +1,27 @@
 #include "system.h"
 #include "led.h"
 #include "navswitch.h"
-
+#include "ir_uart.h"
 
 
 
 int main (void)
 {
     system_init ();
+    ir_uart_init ();
+    int8_t sent = 0;
+    int8_t received = 0;
     if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-            ir_uart_putc (1);
+        ir_uart_putc ('?');
+        sent = 1;
     }
     while (1)
-    { 
-        
+    {
+        if (ir_uart_read_ready_p ())
+        {
+            if (ir_uart_getc () == "?") {
+                received = 1;
+            }
+        }
     }
-
 }
