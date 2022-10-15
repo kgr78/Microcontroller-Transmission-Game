e180da9eca2eb56bbcfcef0a3caebd1669f083dc
Kahu Griffin <kgr78@cs18200kq.canterbury.ac.nz>
2022-10-15 14:21:44 +1300

    adding in recieving and deciding winner in player choice

--- player_choice.c
+++ player_choice.c
@@ -1,11 +1,11 @@
 #include "system.h"
 #include "pacer.h"
 #include "navswitch.h"
-#include "ir_uart.h"
 #include "tinygl.h"
 #include "../fonts/font5x7_1.h"
 #include "player_choice.h"
 #include "led.h" 
+#include "decide_winner.h"
 
 
 #define PACER_RATE 500
@@ -32,21 +32,19 @@ char change_character(int8_t count) {
     return character; 
 }
 
-
-int choose_character(void) {
+char choose_character(void) {
     int8_t count = 0; 
     char character = change_character(count);
+    char r_character; 
     init_screen(); 
-    navswitch_init ();
-    uint8_t chare;
-
-    /* TODO: Initialise IR driver.  */
     ir_uart_init ();
-
+    navswitch_init ();
+    bool chosen = false; 
+    uint8_t chare;
 
     pacer_init (PACER_RATE);
 
-    while (1)
+    while (chosen == false)
     {
         pacer_wait ();
         tinygl_update ();
@@ -58,25 +56,20 @@ int choose_character(void) {
                 character = change_character(count); 
             }
                 
-
         if (navswitch_push_event_p (NAVSWITCH_SOUTH))
             if (count > 0 && count <= 2) {
                 count--;
                 character = change_character(count); 
             }
-
         if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-            ir_uart_putc (character);
-        }
-        if (ir_uart_read_ready_p())
-        {
+            ir_uart_putc(character);
             chare=ir_uart_getc();
             if (chare<127 && chare>32) {
-                character = chare;    
-            }
+                r_character = chare;   
+                result(r_character,character);  
+            } 
         }
         display_character (character);   
-    }
-
-    return 0;
+    } 
+    return character; 
 }
