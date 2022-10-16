026b91194d2f62c67250c41fc92d97b8f621d9b1
Connor Parsons <cpa116@cs18266kq.canterbury.ac.nz>
2022-10-16 19:15:26 +1300

    update game.c

--- game.c
+++ game.c
@@ -2,8 +2,8 @@
 #include "led.h"
 #include "navswitch.h"
 #include "ir_uart.h"
-#include "led_symbols.h"
 #include "player_choice.h"
+#include <stdbool.h>
 //kahu
 
 
@@ -11,24 +11,8 @@ int main (void)
 {
     system_init();
     ir_uart_init();
+    welcome_screen();
     choose_character();
-    // system_init ();
-    // ir_uart_init ();
-    // int8_t sent = 0;
-    // int8_t received = 0;
-    // welcome_screen();
-    // while (sent != 1 && received != 1)
-    // {
-    //     if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
-    //         ir_uart_putc ('?');
-    //         sent = 1;
-    //     }
-    //     if (ir_uart_read_ready_p ())
-    //     {
-    //         if (ir_uart_getc () == "?") {
-    //             received = 1;
-    //         }
-    //     }
-    // }
+
     
 }
